ed32deb397b5ee91b93e1dc9fca9fa158824c33c
Ikayomi <alayodavid28@gmail.com>
2026-04-28 10:33:56 +0100

    adding new component-courses

---
+++ src/components/Courses/Courses.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Courses.css"
+
+const Courses = () => {
+  return (
+    <div>
+          <section className="course">
+        <h1>Courses We Offer</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+
+        <div className="row">
+            <div className="course-col">
+                <h3> Intermediate</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet turpis nulla, eleifend faucibus est sollicitudin ut.Maecenas ut venenatis ex, et dapibus purus Donec sit.</p>
+            </div>
+            <div className="course-col">
+                <h3> Degree</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet turpis nulla, eleifend faucibus est sollicitudin ut.Maecenas ut venenatis ex, et dapibus purus Donec sit.</p>
+            </div>
+            <div className="course-col">
+                <h3> Post Graduation</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet turpis nulla, eleifend faucibus est sollicitudin ut.Maecenas ut venenatis ex, et dapibus purus Donec sit.</p>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Courses
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>
+        <p className="dave"> My name is David Alayo</p>
+    </div>
+  )
+}
+
+export default Navbar
+
